8ecc017cf669b282deefa1315bd95b30c3be6a49
Rune Jensen <rje@venuemanager.net>
2024-10-30 14:47:06 +0100

    rotator via port 9001

--- versions/sphinxsearch-2.2.11/sphinx_conf_updater.sh
+++ versions/sphinxsearch-2.2.11/sphinx_conf_updater.sh
@@ -3,3 +3,7 @@ sed -i -e s/\$SQL_HOST/$SQL_HOST/ /usr/local/etc/sphinx.conf
 sed -i -e s/\$SQL_USER/$SQL_USER/ /usr/local/etc/sphinx.conf
 sed -i -e s/\$SQL_PASS/$SQL_PASS/ /usr/local/etc/sphinx.conf
 sed -i -e s/\$SQL_DB/$SQL_DB/ /usr/local/etc/sphinx.conf
+
+cd /rotator
+. .venv/bin/activate
+flask --app app run --host=0.0.0.0 --port=9001 &
